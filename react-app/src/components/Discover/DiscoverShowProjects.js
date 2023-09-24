e7f6124a30da587f101154cf9b35d2aea6cb4008
Andrew <1andrew.khai@gmail.com>
2023-09-23 19:34:49 -0700

    fixed discoverall bug

--- react-app/src/components/Discover/DiscoverShowProjects.js
+++ react-app/src/components/Discover/DiscoverShowProjects.js
@@ -42,19 +42,19 @@ const DiscoverShowProjects = ({ project }) => {
   return (
     <div className="single-project-discover">
       <div className="single-project-image-div">
-        <NavLink to={`/projects/${project.id}`}>
-          <img src={project.projectImage} style={{ width: "390px", height: "220px" }}></img>
+        <NavLink to={`/projects/${project?.id}`}>
+          <img src={project?.projectImage} style={{ width: "390px", height: "220px" }}></img>
         </NavLink>
       </div>
       <div className="single-project-main-info">
         <div style={{fontSize: "1.1rem", fontWeight: "bold"}}>
-          {project.title}
+          {project?.title}
         </div>
         <div style={{marginTop: "10px", color: "#656969"}}>
-          {project.description}
+          {project?.description}
         </div>
         <div style={{marginTop: "20px", color: "#656969"}}>
-          by {project.creator.username}
+          by {project.creator?.username}
         </div>
       </div>
       <div className="single-project-pledge-info">
@@ -63,7 +63,7 @@ const DiscoverShowProjects = ({ project }) => {
           ${funded(project)} pledged
         </div>
         <div>
-          <div>{Math.ceil((funded(project) / project.fundingGoal) * 100)}% funded</div>
+          <div>{Math.ceil((funded(project) / project?.fundingGoal) * 100)}% funded</div>
         </div>
         <div style={{color: "#656969"}}>
           {differenceInDays < 0 ? 0 : differenceInDays} days to go
@@ -71,10 +71,10 @@ const DiscoverShowProjects = ({ project }) => {
       </div>
       <div className="single-project-other-info">
         <div style={{color: "#656969"}}>
-          <i class="fa-regular fa-compass"></i> {project.category.name}
+          <i class="fa-regular fa-compass"></i> {project?.category.name}
         </div>
         <div style={{color: "#656969"}}>
-          <i class="fa-solid fa-location-dot"></i> {project.location}
+          <i class="fa-solid fa-location-dot"></i> {project?.location}
         </div>
       </div>
     </div>
